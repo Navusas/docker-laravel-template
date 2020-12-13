72aa9af1e7839f975160161317881c8522ad9824
Navusas <u1757704@unimail.hud.ac.uk>
2020-12-13 21:33:03 +0000

    v0.1.0. Resolved nginx and npm containers not loading

--- app.sh
+++ app.sh
@@ -1,16 +1,48 @@
-!/bin/bash
-
-# Author: Domantas Giedraitis
-# Date: 14/11/2020
-# Version: 0.1
+#!/bin/bash
 #
-# Purpose:
-#   This helper class minimizes the effort of containerizing and building the laravel application specified in the src/ file
+#
+# Author:	Domantas Giedraitis
+#
 #
 
+AUTHOR="Domantas Giedraitis"
+SCRIPT=$( basename "$0" )
+VERSION="0.1.0"
+
+# Constants
 LIGHT_PURPLE='\033[1;35m'
 CYAN='\033[0;36m'
 NO_COLOR='\033[0m'
+
+function usage
+{
+  local txt=(
+"$SCIRPT is a Utility tool, which minimizes the effort of containerizing and building the laravel application."
+"Author:	$AUTHOR"
+"Usage: 	$SCRIPT [option]"
+""
+"	!! 1 option per run only 11"
+""
+""
+"Options:"
+"    -h	|	--help				Print this screen"
+"    -v |	--version			List version"
+"    		--start				Start only (Docker Compose Up)"
+"		--full-start			Build and start all containers (takes 5 minutes)"
+"    		--stop				Stop all running containers"
+"    		--prune				Delete images/volumes cache"
+)
+  printf "%s\n" "${txt[@]}"
+}
+
+function version
+{
+  local txt=(
+"$SCRIPT version $VERSION"
+)
+
+  printf "%s\n" "${txt[@]}"
+}
 
 print_header() {
   printf "${CYAN}"
@@ -23,19 +55,19 @@ start_containers() {
 
   printf "${CYAN}"
   printf "\n################################################"
-  printf "\n\tInitializing the project..."
+  printf "\n\tInitializing the project... (This can take up to 5 minutes)"
   printf "\n################################################"
 
-  print_header "Starting-up Docker Containers"
+  print_header "Starting-up Docker Containers..."
   docker-compose build --compress --build-arg key=app --quiet
   docker-compose up -d
 
 
-  print_header "Generating Laravel App Key"
+  print_header "Generating Laravel App Key..."
   docker-compose exec app php artisan key:generate
 
 
-  print_header "Allowing Containers to Load Properly"
+  print_header "Allowing Containers to Load Properly before proceeding..."
   for i in {1..10}
   do
     printf "\r${LIGHT_PURPLE}Waiting for containers to load before seeding the database $((10 - $i))...${NO_COLOR}"
@@ -43,7 +75,7 @@ start_containers() {
   done
 
 
-  print_header "Docker Containers Status"
+  print_header "Docker Containers Status..."
   docker-compose ps
 
 
@@ -51,34 +83,50 @@ start_containers() {
   docker-compose exec app php artisan migrate:fresh --seed
 
 
-  # Let user to glance at the seeding outptu
+  # Let user to glance at the seeding output
   sleep 1s
 
 
   clear -x
-  print_header "MyCarBoot App is Up and Running. Enjoy!\n"
+  print_header "App is Up and Running. Enjoy!\n"
 }
-
-stop_containers() {
-  docker-compose down
-}
-
 
 #########################################
 #    App Starts
 #########################################
-if [[ $1 == "--start" ]]; then
-  start_containers
-elif [[ $1 == "--stop" ]]; then
-  docker-compose down
-elif [[ $1 == "--remove-all" ]]; then
-    read -p "Are you sure? This will stop and remove ALL your containers [y/N] " -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-              all_containers=$(docker ps -a -q)
-        for i in ${all_containers[@]}; do
-            docker stop $i
-            docker rm $i
-        done
-    fi
-fi
+
+while (( $# ))
+do
+  case "$1" in
+
+    --full-start)
+      start_containers
+      exit 0
+    ;;
+
+    --start)
+      docker-compose up
+      exit 0
+    ;;
+
+    --stop)
+      docker-compose down
+      exit 0
+    ;;
+
+    --prune)
+      docker system prune -a
+      exit 0
+    ;;
+
+    --help | -h)
+      usage
+      exit 0
+    ;;
+
+    --version | -v)
+      version
+      exit 0
+    ;;
+  esac
+done
--- node.dockerfile
+++ node.dockerfile
@@ -3,14 +3,15 @@ FROM node:15.2-alpine
 WORKDIR /var/www
 
 # Create layer to cache the occasionally changing dependencies
-COPY ./package*.json ./
+COPY ./src/package*.json ./
 
 RUN npm install
-RUN npm install -g nodemon
+#RUN npm install -g nodemon
 
 ENV NODE_ENV=development
 
 EXPOSE 9090
 
-CMD ["nodemon", "-L"]
+CMD ["npm", "run", "dev"]
+#CMD ["nodemon", "-L"]
 
